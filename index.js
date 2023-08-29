16e8be25a9c4d7c0d3984b080220c8e8dcfc2522
Erin Wise <erinwise88@gmail.com>
2023-08-28 23:44:30 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+// Add your code herfu
+
+
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        name: userName,
+        email: userEmail
+      }) 
+      
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.id)
+        let body = document.querySelector('body')
+        //let element = document.createElement('p')
+        body.textContent = data.id
+        //body.appendChild(element)
+        //element.innerText = data.id
+    }) 
+    .catch(error => {
+        const body = document.querySelector('body');
+        body.textContent = error.message;
+    })
+}
+
+
